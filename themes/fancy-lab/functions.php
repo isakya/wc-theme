7ef9a85436a6c46d0f39315393f31ff43f453420
isakya <1016361268@qq.com>
2024-05-05 09:11:25 +0800

    页面添加加载钩子、动态版本号防止浏览器缓存

--- themes/fancy-lab/functions.php
+++ themes/fancy-lab/functions.php
@@ -1,6 +1,6 @@
 <?php
 
 function fancy_lab_scripts() {
-    wp_enqueue_style('fancy-lab-style', get_stylesheet_uri(), array(), '1.0', 'all');
+    wp_enqueue_style('fancy-lab-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all');
 }
 add_action('wp_enqueue_scripts', 'fancy_lab_scripts');
