ceadb685afa2b906fce761a8abf5c283f690ce07
isakya <1016361268@qq.com>
2024-05-05 22:50:59 +0800

    创建page.php模板

--- themes/fancy-lab/page.php
+++ themes/fancy-lab/page.php
@@ -8,7 +8,7 @@
                         while (have_posts()): the_post();
                             ?>
                             <article>
-                                <h2><?php the_title(); ?></h2>
+                                <h1><?php the_title(); ?></h1>
                                 <div><?php the_content(); ?></div>
                             </article>
                         <?php
