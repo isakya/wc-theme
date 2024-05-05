0eb1bdc3a60e3c382f87436ac3e25507c56748dd
isakya <1016361268@qq.com>
2024-05-06 02:01:55 +0800

    woocommerce主题授权、设置产品详情展示页

--- themes/fancy-lab/functions.php
+++ themes/fancy-lab/functions.php
@@ -11,11 +11,33 @@ add_action('wp_enqueue_scripts', 'fancy_lab_scripts');
 
 
 function fancy_lab_config() {
+    // woocommerce主题授权，并修改基本参数
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 255,
+        'single_image_width' => 255,
+        'product_grid' => array(
+            'default_rows' => 10,
+            'min_rows' => 5,
+            'max_rows' => 10,
+            'default_columns' => 1,
+            'min_columns' => 1,
+            'max-columns' => 1
+        )
+    ) );
+    add_theme_support('wc-product-gallery-zoom'); // 产品主图放大
+    add_theme_support('wc-product-gallery-lightbox'); // 产品主图tab
+    add_theme_support('wc-product-gallery-slider'); // 产品主图滑动
+
+
+    if ( ! isset( $content_width ) ) {
+        $content_width = 600;
+    }
     register_nav_menus(
       array(
           'fancy_lab_main_menu' => 'Fancy Lab Main Menu',
           'fancy_lab_footer_menu' => 'Fancy Lab Footer Menu'
       )
     );
+
 }
 add_action('after_setup_theme', 'fancy_lab_config', 0);
