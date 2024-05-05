311ef91af84c5a2fe024c7ce994da1990abc85a2
isakya <1016361268@qq.com>
2024-05-05 17:51:38 +0800

    添加bootstrap库文件并加载进网页

--- themes/fancy-lab/functions.php
+++ themes/fancy-lab/functions.php
@@ -1,6 +1,10 @@
 <?php
 
-function fancy_lab_scripts() {
+function fancy_lab_scripts()
+{
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array('jquery'), '5.3.0',true);
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '5.3.0', 'all');
     wp_enqueue_style('fancy-lab-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all');
 }
+
 add_action('wp_enqueue_scripts', 'fancy_lab_scripts');
