970606dbc8372db3fcc59602f3c620242583e3cd
isakya <1016361268@qq.com>
2024-05-05 22:42:51 +0800

    创建通用模板（必须要注释模板名称），创建后可在page编辑页面指定使用模板

--- themes/fancy-lab/template-home.php
+++ themes/fancy-lab/template-home.php
@@ -1,6 +1,33 @@
-<?php get_header(); ?>
+<?php
+
+/*
+    Template Name: Home Page
+ */
+
+get_header(); ?>
+
     <div class="content-area">
         <main>
+            <section class="slider">
+                <div class="container">
+                    <div class="row">Slider</div>
+                </div>
+            </section>
+            <section class="popular-products">
+                <div class="container">
+                    <div class="row">Popular Products</div>
+                </div>
+            </section>
+            <section class="new-arrivals">
+                <div class="container">
+                    <div class="row">New Arrivals</div>
+                </div>
+            </section>
+            <section class="deal-of-the-week">
+                <div class="container">
+                    <div class="row">Deal of the Week</div>
+                </div>
+            </section>
             <section class="lab-blog">
                 <div class="container">
                     <div class="row">
