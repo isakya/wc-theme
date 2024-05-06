65bb822e54867c0add83c63655a402beaf88b712
isakya <1016361268@qq.com>
2024-05-07 02:20:54 +0800

    使用谷歌字体

--- themes/fancy-lab/functions.php
+++ themes/fancy-lab/functions.php
@@ -5,6 +5,9 @@ function fancy_lab_scripts()
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array('jquery'), '5.3.0',true);
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '5.3.0', 'all');
     wp_enqueue_style('fancy-lab-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all');
+
+    // 谷歌字体
+    wp_enqueue_style('google-fonts', 'https://fonts.gooleapis.com/css?family=Rajdhani:400,500,600,700|https://fonts.gooleapis.com/css?family=Seaweed+Script');
 }
 
 add_action('wp_enqueue_scripts', 'fancy_lab_scripts');
